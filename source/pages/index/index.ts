cb9481d596eded3c963402b7876e82de4eda4830
Agustin Soria <agustinesoria96@gmail.com>
2016-09-24 11:40:23 -0300

    Optimizacion y mejoras de diseño
    Index: banner y carrusel cambiados, footer cambiado
    Servicios: tabla cambiada
    General: backgrounds blancos y letras negras

--- source/pages/index/index.ts
+++ source/pages/index/index.ts
@@ -5,6 +5,7 @@ $(document).ready(function ($) {
         pagination: '.swiper-pagination',
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
+        speed: 800,
         paginationClickable: true,
         spaceBetween: 30,
         centeredSlides: true,
--- source/pages/servicios/servicios.ts
+++ source/pages/servicios/servicios.ts
@@ -16,20 +16,4 @@ $('.navegacion').find('a').click(function (e) {
   $("html, body").animate({ scrollTop: $(direccion).offset().top }, 600);
   return false;
 });
-  // $(function() {
-	// $('div.navegacion a').bind('click',function(event){
-	// 	var $anchor = $(this);
-
-	// 	$('html, body').stop().animate({
-	// 		scrollTop: $($anchor.attr('href')).offset().top
-	// 	}, 1500,'easeInOutExpo');
-		/*
-		if you don't want to use the easing effects:
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1000);
-		*/
-		// event.preventDefault();
-// 	});
-// });
 });
