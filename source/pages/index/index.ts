7b1ccf267ca198e0600b914a8df9f790d4ad1a15
Agustin Soria <agustinesoria96@gmail.com>
2016-11-28 20:00:20 -0300

    Mapa arreglado

--- source/pages/index/index.ts
+++ source/pages/index/index.ts
@@ -13,26 +13,26 @@ $(document).ready(function ($) {
     });
 
 
-  function initMap() {
-    var city = 'Productores Asociados - Seguros Generales';
-    var myLatLng = { lat: -32.413980, lng: -63.247759 };
-    var map = new google.maps.Map(document.getElementById('googleMap'), {
-      scrollwheel: false,
-      center: myLatLng,
-      zoom: 18
-    });
-    var infowindow = new google.maps.InfoWindow({
-      content: '<strong>Ubicación:</strong><br>Buenos Aires 660<br> Villa María<br>'
-    });
-    var marker = new google.maps.Marker({
-      map: map,
-      position: myLatLng,
-      title: city
-    });
-    google.maps.event.addListener(marker, 'click', function () {
-      infowindow.open(map, marker);
-    });
-    infowindow.open(map, marker);
-  }
-  google.maps.event.addDomListener(window, 'load', initMap);
-});
+//   function initMap() {
+//     var city = 'Productores Asociados - Seguros Generales';
+//     var myLatLng = { lat: -32.413980, lng: -63.247759 };
+//     var map = new google.maps.Map(document.getElementById('googleMap'), {
+//       scrollwheel: false,
+//       center: myLatLng,
+//       zoom: 18
+//     });
+//     var infowindow = new google.maps.InfoWindow({
+//       content: '<strong>Ubicación:</strong><br>Buenos Aires 660<br> Villa María<br>'
+//     });
+//     var marker = new google.maps.Marker({
+//       map: map,
+//       position: myLatLng,
+//       title: city
+//     });
+//     google.maps.event.addListener(marker, 'click', function () {
+//       infowindow.open(map, marker);
+//     });
+//     infowindow.open(map, marker);
+//   }
+//   google.maps.event.addDomListener(window, 'load', initMap);
+// });
